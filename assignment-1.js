62d4dd7ace4989a4668273000468cda7f5b802e8
silpagui <silpagui.sa@gmail.com>
2022-01-27 17:41:37 +0100

    counter assignment

---
+++ assignment-1.js
@@ -0,0 +1,36 @@
+const timeoutBtn = document.querySelector(".timeout");
+const intervalBtn = document.querySelector(".interval");
+const container = document.querySelector(".container");
+
+function setTimeoutSolution() {
+	let counter = 0;
+	return function loop() {
+		if (counter >= 1000000) {
+			console.log("Set Timeout Solution Finished!");
+			return counter;
+		}
+		setTimeout(loop, 0);
+		console.log("Set Timeout: ", ++counter);
+	};
+}
+const loop = setTimeoutSolution();
+
+timeoutBtn.addEventListener("click", () => {
+	loop();
+});
+
+function myIntervalSolution() {
+	let counter = 0;
+	const id = setInterval(() => {
+		if (counter >= 1000000) {
+			console.log(" Set Interval Solution Finished!");
+			clearInterval(id);
+		} else {
+			console.log("Set Interval: ", ++counter);
+		}
+	}, 0);
+}
+
+intervalBtn.addEventListener("click", () => {
+	myIntervalSolution();
+});
