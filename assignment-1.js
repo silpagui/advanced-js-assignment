52685cd0c80e90f8e95b96ea15860c992e5281d9
silpagui <silpagui.sa@gmail.com>
2022-01-28 11:24:11 +0100

    fix spaces

--- assignment-1.js
+++ assignment-1.js
@@ -3,34 +3,35 @@ const intervalBtn = document.querySelector(".interval");
 const container = document.querySelector(".container");
 
 function setTimeoutSolution() {
-	let counter = 0;
-	return function loop() {
-		if (counter >= 1000000) {
-			console.log("Set Timeout Solution Finished!");
-			return counter;
-		}
-		setTimeout(loop, 0);
-		console.log("Set Timeout: ", ++counter);
-	};
+  let counter = 0;
+  return function loop() {
+    if (counter >= 1000000) {
+      console.log("Set Timeout Solution Finished!");
+      return counter;
+    }
+    setTimeout(loop, 0);
+    console.log("Set Timeout: ", ++counter);
+  };
 }
+
 const loop = setTimeoutSolution();
 
 timeoutBtn.addEventListener("click", () => {
-	loop();
+  loop();
 });
 
 function myIntervalSolution() {
-	let counter = 0;
-	const id = setInterval(() => {
-		if (counter >= 1000000) {
-			console.log(" Set Interval Solution Finished!");
-			clearInterval(id);
-		} else {
-			console.log("Set Interval: ", ++counter);
-		}
-	}, 0);
+  let counter = 0;
+  const id = setInterval(() => {
+    if (counter >= 1000000) {
+      console.log(" Set Interval Solution Finished!");
+      clearInterval(id);
+    } else {
+      console.log("Set Interval: ", ++counter);
+    }
+  }, 0);
 }
 
 intervalBtn.addEventListener("click", () => {
-	myIntervalSolution();
+  myIntervalSolution();
 });
